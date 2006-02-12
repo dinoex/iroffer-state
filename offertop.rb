d49ecb1ccc61417f6197fdad5dc9ea4d595bfa90
dm <dm>
2006-02-12 15:59:54 +0000

    - neues UI
    ./offertot.rb mybot.state < log
    ./offertot.rb mybot.state mybot.log
    ./offertot.rb mybot.log

--- offertop.rb
+++ offertop.rb
@@ -1,6 +1,6 @@
 #!/usr/local/bin/ruby -w
 #
-#	$Id: offertop.rb,v 1.4 2006/02/12 14:54:45 dm Exp $
+#	$Id: offertop.rb,v 1.5 2006/02/12 15:59:54 dm Exp $
 #	(c) 2005, Dirk Meyer, Im Grund 4, 34317 Habichtswald
 #	based on wirk from:;
 #	(C) 2002 by dpunkt.de, Armin Roehrl, Stefan Schmiedl, Clemens Wyss 2002-01-20
@@ -64,12 +64,12 @@ end
 
 # ** 2006-02-10-06:17:25: XDCC SEND #29 requested: ihs (euirc-13824fe7.bpool.celox.de)
 # ** 2006-02-10-17:48:48: XDCC SEND 15 Queued (slot): Spaghetti (euirc-6ab9fef9.adsl.alicedsl.de)
-request_pack = / XDCC SEND #*([0-9]*) (?:requested|Queued .slot.): [^ ]* /
-request_nick = / XDCC SEND #*[0-9]* (?:requested|Queued .slot.): ([^ ]*) /
+$request_pack = / XDCC SEND #*([0-9]*) (?:requested|Queued .slot.): [^ ]* /
+$request_nick = / XDCC SEND #*[0-9]* (?:requested|Queued .slot.): ([^ ]*) /
 # ** 2006-02-10-06:17:26: XDCC [515:ihs]: Connection established (84.245.180.163:1027 -> 213.239.196.229:53686)
-connected_nick = / XDCC [\[][0-9]*[:]([^\]]*)[\]]: Connection established /
+$connected_nick = / XDCC [\[][0-9]*[:]([^\]]*)[\]]: Connection established /
 # ** 2006-02-10-06:39:30: XDCC [515:ihs]: Transfer Completed (383302 KB, 22 min 3.865 sec, 289.5 KB/sec)
-completed_nick = / XDCC [\[][0-9]*[:]([^\]]*)[\]]: Transfer Completed /
+$completed_nick = / XDCC [\[][0-9]*[:]([^\]]*)[\]]: Transfer Completed /
 #completed_speed = / XDCC [\[][0-9]*[:][^\]]*[\]]: Transfer Completed .[0-9]* KB, [^,]*, ([0-9]*[.][0-9]) /
 # ** 2006-02-10-06:18:02: Stat: 1/20 Sls, 0/20 Q, 2464.8K/s Rcd, 0 SrQ (Bdw: 8100K, 67.5K/s, 2473.1K/s Rcd)
 
@@ -77,7 +77,7 @@ $packs = Array.new(0)
 
 # Die Gruppierungen im regulären Ausdruck
 # werden in den LogHashtabellen gezählt
-def log_search( *hash )
+def log_search( input, *hash )
 	# String-Objekt aus der Schleife ziehen
 	split = "\n"
 	# hash-Indizes an MatchData angleichen
@@ -85,8 +85,8 @@ def log_search( *hash )
 
 	loop {
 		# nächster Block und der Rest der Zeile
-		data = STDIN.read(4095) or break
-		data += (STDIN.gets || "")
+		data = input.read(4095) or break
+		data += (input.gets || "")
 
 		for line in data.split(split)
 			for i in 1...hash.length
@@ -164,15 +164,45 @@ def parse_buffer(buffer, bsize)
 	end
 end
 
+def parse_state( filename )
+	File.stat(filename).file? or next
+	bsize = File.size(filename)
+	begin
+		buffer = File.open(filename, 'r').read
+		parse_buffer( buffer, bsize )
+	rescue
+		$stderr.print "Failure at #{filename}: #{$!} => Skipping!\n"
+	end
+end
+
+def make_statistik( input )
+	return log_search( input,
+		LogHashPack.new( $request_pack, 'Pack', 'Anfragen' ),
+		LogHash.new( $request_nick, 'Nick', 'Anfragen' ),
+		LogHash.new( $connected_nick, 'Nick', 'Verbindungen' ),
+		LogHash.new( $completed_nick, 'Nick', 'Downloads' )
+		)
+end
+
+input = 0
 if ARGV.size > 0 then
 	ARGV.each { |filename|
-		File.stat(filename).file? or next
-		bsize = File.size(filename)
-		begin
-			buffer = File.open(filename, 'r').read
-			parse_buffer( buffer, bsize )
-		rescue
-			$stderr.print "Failure at #{filename}: #{$!} => Skipping!\n"
+		if /[.]state/.match( filename )
+			parse_state( filename )
+			next
+		end
+		if /[.]log/.match( filename )
+			if ( $packs.size == 0 )
+				state = filename.sub( /[.].*$/, '' )
+				parse_state( "#{state}.state" )
+			end
+			input += 1
+			if input == 1
+				$list = make_statistik( File.open(filename, 'r') )
+				next
+			end
+			STDERR.print "Only 1 Logfile supported!\n"
+			exit 1
 		end
 	}
 else
@@ -180,14 +210,11 @@ else
 	exit 1
 end
  
-list = log_search(
-	LogHashPack.new( request_pack, 'Pack', 'Anfragen' ),
-	LogHash.new( request_nick, 'Nick', 'Anfragen' ),
-	LogHash.new( connected_nick, 'Nick', 'Verbindungen' ),
-	LogHash.new( completed_nick, 'Nick', 'Downloads' )
-	)
+if input == 0
+	$list = make_statistik( STDIN )
+end
 
-list.each { |top|
+$list.each { |top|
 	top.printtop( 10 )
 }
 
